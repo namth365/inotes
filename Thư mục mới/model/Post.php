669fa7586edf4b0225994aab4653e5093aef3c1c
namth365 <namth365@gmail.com>
2021-10-13 16:28:44 +0700

    Abv

---
+++ Thư mục mới/model/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Model;
+
+class Post
+{
+    public $id;
+    public $name;
+    public $description;
+
+    public function __construct($name, $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
+}
